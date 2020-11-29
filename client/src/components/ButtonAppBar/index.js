47010ee184d25778be42e2d02d5026712a0aeb95
Kory Nelson <kory.nelson99@gmail.com>
2020-11-29 09:56:06 -0800

    link menu buttons to router to navigate to separate pages

--- client/src/components/ButtonAppBar/index.js
+++ client/src/components/ButtonAppBar/index.js
@@ -3,9 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { withRouter} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,20 +17,58 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
   },
 }));
 
-export default function ButtonAppBar() {
+const MenuAppBar = (props) => {
+  const {history} = props;
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClick= (url) => {
+    history.push(url)
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <MenuIcon 
+              onClick={handleMenu}
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            />
           </IconButton>
+          <div>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                <MenuItem onClick={() => handleClick('/')}>Search</MenuItem>
+                <MenuItem onClick={() => handleClick('/savedbooks')}>Saved Books</MenuItem>
+              </Menu>
+            </div>
           <Typography variant="h6" className={classes.title}>
             Book Search
           </Typography>
@@ -37,3 +77,5 @@ export default function ButtonAppBar() {
     </div>
   );
 }
+
+export default withRouter(MenuAppBar);
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
     <React.StrictMode>
       <Router>
-        <CssBaseline />
         <App />
       </Router>
     </React.StrictMode>,
