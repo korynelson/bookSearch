b79344e9d6807a2672adfe34adcc9cad5829b139
Kory Nelson <kory.nelson99@gmail.com>
2020-11-29 11:22:03 -0800

    added models and seed.js to seed the databse with 1 book

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { 
+      type: String, 
+      required: true 
+    },
+  authors: {
+      type: Array,
+      required: true
+    },
+  img: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
